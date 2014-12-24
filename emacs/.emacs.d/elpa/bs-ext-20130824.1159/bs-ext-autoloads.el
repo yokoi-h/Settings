78accaa6cc6b7032a11daa1bb281ba8a1fdf844b
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2014-12-24 14:32:52 +0900

    add emacs.d

---
+++ emacs/.emacs.d/elpa/bs-ext-20130824.1159/bs-ext-autoloads.el
@@ -0,0 +1,15 @@
+;;; bs-ext-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("bs-ext.el") (21655 47586 451872 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; bs-ext-autoloads.el ends here
---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,63 @@
+(load-theme 'manoj-dark t)
+
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+
+;;; load-pathを追加する関数を定義
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+     (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+             (normal-top-level-add-subdirs-to-load-path))))))
+
+;;; ディレクトリをサブディレクトリごとload-pathに追加
+(add-to-load-path "elisp")
+
+(autoload 'gtags-mode "gtags" "" t)
+(setq gtags-mode-hook
+      '(lambda ()
+         (local-set-key "\M-t" 'gtags-find-tag)
+         (local-set-key "\M-r" 'gtags-find-rtag)
+         (local-set-key "\M-s" 'gtags-find-symbol)
+         (local-set-key "\C-t" 'gtags-pop-stack)
+         ))
+
+(add-hook 'c-mode-common-hook
+          '(lambda()
+             (gtags-mode 1)
+             (gtags-make-complete-list)
+             ))
+;;; go-mode
+(require 'go-mode)
+(add-to-list 'exec-path (expand-file-name "~/workspace/go/bin"))
+(defun my-go-mode-hook ()
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+;;emacs_nav
+(require 'nav)
+(defun nav-mode-hl-hook ()
+  (local-set-key (kbd "<right>") 'nav-open-file-under-cursor)
+  (local-set-key (kbd "<left>")  'nav-go-up-one-dir)
+  )
+ 
+(add-hook 'nav-mode-hook 'nav-mode-hl-hook)
+
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+
+(global-set-key (kbd "C-x C-b") 'bs-show)
+
+(xterm-mouse-mode t)
+(mouse-wheel-mode t)
+(global-set-key   [mouse-4] '(lambda () (interactive) (scroll-down 1)))
+(global-set-key   [mouse-5] '(lambda () (interactive) (scroll-up   1)))
+
