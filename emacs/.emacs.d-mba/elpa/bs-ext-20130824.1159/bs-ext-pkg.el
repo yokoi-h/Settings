4745ce07743899bc32e7687193a887c86f2a4e5e
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2014-12-26 10:47:14 +0900

    add mba setting

---
+++ emacs/.emacs.d-mba/elpa/bs-ext-20130824.1159/bs-ext-pkg.el
@@ -0,0 +1,1 @@
+(define-package "bs-ext" "20130824.1159" "Extensions to emacs buffer-selection library (bs.el)" 'nil :url "http://www.emacswiki.org/emacs/download/bs-ext.el" :keywords '("convenience" "files"))
