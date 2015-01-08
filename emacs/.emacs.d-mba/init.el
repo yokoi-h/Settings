14b17a9a71e7ad456b45f5c940584905aa2e7191
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2015-01-08 18:44:21 +0900

    add key for grep-find and xterm-mouse-mode

--- emacs/.emacs.d-mba/init.el
+++ emacs/.emacs.d-mba/init.el
@@ -61,3 +61,5 @@
 (global-set-key   [mouse-4] '(lambda () (interactive) (scroll-down 1)))
 (global-set-key   [mouse-5] '(lambda () (interactive) (scroll-up   1)))
 
+(global-set-key (kbd "C-x /") 'grep-find)
+(global-set-key (kbd "C-x ,") 'xterm-mouse-mode)
